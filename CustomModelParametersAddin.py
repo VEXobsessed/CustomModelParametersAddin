90586c8bcba786009cf0069692bb96aeeb84908b
VEXObsessed <49462576+vexobsessed@users.noreply.github.com>
2020-06-08 09:09:55 -0500

    Removed commented out code

--- CustomModelParametersAddin.py
+++ CustomModelParametersAddin.py
@@ -5,7 +5,9 @@ import adsk.core, adsk.fusion, adsk.cam, traceback
 
 app = None
 ui  = None
-_rowNumber = 0
+global eventArgs
+global inputs
+global cmdInput
 # selectionInput = None
 globalComp = None
 globalSize = None
@@ -13,34 +15,22 @@ globalSize = None
 # Global set of event handlers to keep them referenced for the duration of the command
 _handlers = []
 
-# Adds a new row to the table.
-def addRowToTable(tableInput):
-    # Get the CommandInputs object associated with the parent command.
-    cmdInputs = adsk.core.CommandInputs.cast(tableInput.commandInputs)
-    
-    # Create three new command inputs.
-    valueInput = cmdInputs.addValueInput('TableInput_value{}'.format(_rowNumber), 'Value', 'cm', adsk.core.ValueInput.createByReal(_rowNumber))
-    stringInput =  cmdInputs.addStringValueInput('TableInput_string{}'.format(_rowNumber), 'String', str(_rowNumber))
-    spinnerInput = cmdInputs.addIntegerSpinnerCommandInput('spinnerInt{}'.format(_rowNumber), 'Integer Spinner', 0 , 100 , 2, int(_rowNumber))
-    
-    # Add the inputs to the table.
-    row = tableInput.rowCount
-    tableInput.addCommandInput(valueInput, row, 0)
-    tableInput.addCommandInput(stringInput, row, 1)
-    tableInput.addCommandInput(spinnerInput, row, 2)
-    
-    # Increment a counter used to make each row unique.
-    # global _rowNumber
-    _rowNumber = _rowNumber + 1
-
-
 def updateParameters():
-    # selectionInput = selectionInput.selection(0)
-    # paramEntity = paramSelection.entity
-    # # paramEntity.name
-    # ui.messageBox(paramEntity.name)
-    ui.messageBox(globalComp.name)
-    # pass
+    sizeInput = inputs.itemById('intSlider')
+    selectionInput = inputs.itemById('selection')
+
+    if cmdInput.id == 'selection':
+        sizeInput.isVisible = bool(selectionInput.selectionCount)
+        if selectionInput.selectionCount > 0:
+            global globalComp
+            globalComp = selectionInput.selection(0).entity.component
+            sizeInput.expressionOne = str(int(globalComp.modelParameters.item(1).value))
+
+    # sizeInput.isVisible = True
+    # if cmdInput.id == 'intSlider':
+    global globalSize
+    globalSize = sizeInput.valueOne
+
 
 
 # Event handler that reacts to any changes the user makes to any of the command inputs.
@@ -49,34 +39,15 @@ class MyCommandInputChangedHandler(adsk.core.InputChangedEventHandler):
         super().__init__()
     def notify(self, args):
         try:
-            # ui.messageBox('Changed')
-            # updateParameters()
+            global eventArgs
+            global inputs
+            global cmdInput
             eventArgs = adsk.core.InputChangedEventArgs.cast(args)
             inputs = eventArgs.inputs
             cmdInput = eventArgs.input
-               
-            selectionInput = inputs.itemById('selection')
-            if cmdInput.id == 'selection':
-                # ui.messageBox(str(selectionInput.selectionCount))
-                if selectionInput.selectionCount > 0:
-                    # ui.messageBox('It works:\n{}'.format(selectionInput.selection(0).entity.component.name))
-                    global globalComp
-                    globalComp = selectionInput.selection(0).entity.component
-                    # ui.messageBox(globalComp.name)
-                    # updateParameters()
 
-            sizeInput = inputs.itemById('intSlider')
-            # if cmdInput.id == 'intSlider':
-            global globalSize
-            globalSize = sizeInput.valueOne
+            updateParameters()
 
-
-                # addRowToTable(tableInput)
-            # elif cmdInput.id == 'tableDelete':
-            #     if tableInput.selectedRow == -1:
-            #         ui.messageBox('Select one row to delete.')
-            #     else:
-            #         tableInput.deleteRow(tableInput.selectedRow)
         except:
             ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
 
@@ -101,6 +72,7 @@ class MyCommandCreatedHandler(adsk.core.CommandCreatedEventHandler):
         super().__init__()
     def notify(self, args):
         try:
+            
             # Get the command that was created.
             cmd = adsk.core.Command.cast(args.command)
 
@@ -118,159 +90,24 @@ class MyCommandCreatedHandler(adsk.core.CommandCreatedEventHandler):
             inputs = cmd.commandInputs
 
             # Create a tab input.
-            tabCmdInput1 = inputs.addTabCommandInput('tab_1', 'Tab 1')
-            tab1ChildInputs = tabCmdInput1.children
-
-            # # Create a read only textbox input.
-            # tab1ChildInputs.addTextBoxCommandInput('readonly_textBox', 'Text Box 1', 'This is an example of a read-only text box.', 2, True)
-
-            # # Create an editable textbox input.
-            # tab1ChildInputs.addTextBoxCommandInput('writable_textBox', 'Text Box 2', 'This is an example of an editable text box.', 2, False)
+            # tabCmdInput1 = inputs.addTabCommandInput('tab_1', 'Tab 1')
+            tab1ChildInputs = inputs
             
             # # Create a message that spans the entire width of the dialog by leaving out the "name" argument.
             # message = '<div align="center">A "full width" message using <a href="http:fusion360.autodesk.com">html.</a></div>'
             # tab1ChildInputs.addTextBoxCommandInput('fullWidth_textBox', '', message, 1, True)            
 
             # Create a selection input.
-            selectionInput = tab1ChildInputs.addSelectionInput('selection', 'Select', 'Component to select')
+            selectionInput = tab1ChildInputs.addSelectionInput('selection', 'Select Parametric Part', 'Component to select')
             selectionInput.setSelectionLimits(1,1)
             selectionFilter = selectionInput.addSelectionFilter("Occurrences")
             # selectionInput.selectionFilters = selectionFilter
 
-            # Create a string value input.
-            # strInput = tab1ChildInputs.addStringValueInput('string', 'Text', 'Basic string command input')
-
-            # # Create value input.
-            # tab1ChildInputs.addValueInput('value', 'Value', 'cm', adsk.core.ValueInput.createByReal(0.0))
-
-            # # Create bool value input with checkbox style.
-            # tab1ChildInputs.addBoolValueInput('checkbox', 'Checkbox', True, '', False)
-
-            # # Create bool value input with button style that can be clicked.
-            # tab1ChildInputs.addBoolValueInput('buttonClick', 'Click Button', False, 'resources/button', True)
-
-            # # Create bool value input with button style that has a state.
-            # tab1ChildInputs.addBoolValueInput('buttonState', 'State Button', True, 'resources/button', True)
-
-            # # Create float slider input with two sliders.
-            # tab1ChildInputs.addFloatSliderCommandInput('floatSlider', 'Float Slider', 'cm', 0, 10.0, True)
-
-            # # Create float slider input with two sliders and a value list.
-            # floatValueList = [1.0, 3.0, 4.0, 7.0]
-            # tab1ChildInputs.addFloatSliderListCommandInput('floatSlider2', 'Float Slider 2', 'cm', floatValueList)
-
-            # # Create float slider input with two sliders and visible texts.
-            # floatSlider3 = tab1ChildInputs.addFloatSliderCommandInput('floatSlider3', 'Float Slider 3', '', 0, 50.0, False)
-            # floatSlider3.setText('Min', 'Max')
-
             # Create integer slider input with one slider.
-            sliderOne = tab1ChildInputs.addIntegerSliderCommandInput('intSlider', 'Integer Slider', 1, 35)         
-            sliderOne.valueOne = 7
+            sliderOne = tab1ChildInputs.addIntegerSliderCommandInput('intSlider', 'Length', 1, 35)
+            sliderOne.minimumValue = 20
+            sliderOne.isVisible = False
 
-            # # Create integer slider input with two sliders and a value list.
-            # tab1ChildInputs.addIntegerSliderListCommandInput('intSlider2', 'Integer Slider 2', valueList)
-
-            # # Create float spinner input.
-            # tab1ChildInputs.addFloatSpinnerCommandInput('spinnerFloat', 'Float Spinner', 'cm', 0.2 , 9.0 , 2.2, 1)
-
-            # # Create integer spinner input.
-            # tab1ChildInputs.addIntegerSpinnerCommandInput('spinnerInt', 'Integer Spinner', 2 , 9 , 2, 3)
-
-            # # Create dropdown input with checkbox style.
-            # dropdownInput = tab1ChildInputs.addDropDownCommandInput('dropdown', 'Dropdown 1', adsk.core.DropDownStyles.CheckBoxDropDownStyle)
-            # dropdownItems = dropdownInput.listItems
-            # dropdownItems.add('Item 1', False, 'resources/One')
-            # dropdownItems.add('Item 2', False, 'resources/Two')
-
-            # # Create dropdown input with icon style.
-            # dropdownInput2 = tab1ChildInputs.addDropDownCommandInput('dropdown2', 'Dropdown 2', adsk.core.DropDownStyles.LabeledIconDropDownStyle);
-            # dropdown2Items = dropdownInput2.listItems
-            # dropdown2Items.add('Item 1', True, 'resources/One')
-            # dropdown2Items.add('Item 2', False, 'resources/Two')
-
-            # # Create dropdown input with radio style.
-            # dropdownInput3 = tab1ChildInputs.addDropDownCommandInput('dropdown3', 'Dropdown 3', adsk.core.DropDownStyles.LabeledIconDropDownStyle);
-            # dropdown3Items = dropdownInput3.listItems
-            # dropdown3Items.add('Item 1', True, '')
-            # dropdown3Items.add('Item 2', False, '')
-
-            # # Create dropdown input with test list style.
-            # dropdownInput4 = tab1ChildInputs.addDropDownCommandInput('dropdown4', 'Dropdown 4', adsk.core.DropDownStyles.TextListDropDownStyle);
-            # dropdown4Items = dropdownInput4.listItems
-            # dropdown4Items.add('Item 1', True, '')
-            # dropdown4Items.add('Item 2', False, '')
-
-            # # Create single selectable button row input.
-            # buttonRowInput = tab1ChildInputs.addButtonRowCommandInput('buttonRow', 'Single Select Buttons', False)
-            # buttonRowInput.listItems.add('Item 1', False, 'resources/One')
-            # buttonRowInput.listItems.add('Item 2', False, 'resources/Two')
-
-            # # Create multi selectable button row input.
-            # buttonRowInput2 = tab1ChildInputs.addButtonRowCommandInput('buttonRow2', 'Multi-select Buttons', True)
-            # buttonRowInput2.listItems.add('Item 1', False, 'resources/One')
-            # buttonRowInput2.listItems.add('Item 2', False, 'resources/Two')
-
-            # # Create tab input 2
-            # tabCmdInput2 = inputs.addTabCommandInput('tab_2', 'Tab 2')
-            # tab2ChildInputs = tabCmdInput2.children
-
-            # # Create group input.
-            # groupCmdInput = tab2ChildInputs.addGroupCommandInput('group', 'Group')
-            # groupCmdInput.isExpanded = True
-            # groupCmdInput.isEnabledCheckBoxDisplayed = True
-            # groupChildInputs = groupCmdInput.children
-            
-            # # Create radio button group input.
-            # radioButtonGroup = groupChildInputs.addRadioButtonGroupCommandInput('radioButtonGroup', 'Radio button group')
-            # radioButtonItems = radioButtonGroup.listItems
-            # radioButtonItems.add("Item 1", False)
-            # radioButtonItems.add("Item 2", False)
-            # radioButtonItems.add("Item 3", False)
-            
-            # # Create image input.
-            # groupChildInputs.addImageCommandInput('image', 'Image', "resources/image.png")
-            
-            # # Create direction input 1.
-            # directionCmdInput = tab2ChildInputs.addDirectionCommandInput('direction', 'Direction1')
-            # directionCmdInput.setManipulator(adsk.core.Point3D.create(0, 0, 0), adsk.core.Vector3D.create(1, 0, 0))
-            
-            # # Create direction input 2.
-            # directionCmdInput2 = tab2ChildInputs.addDirectionCommandInput('direction2', 'Direction 2', 'resources/One')
-            # directionCmdInput2.setManipulator(adsk.core.Point3D.create(0, 0, 0), adsk.core.Vector3D.create(0, 1, 0)) 
-            # directionCmdInput2.isDirectionFlipped = True
-            
-            # # Create distance value input 1.
-            # distanceValueInput = tab2ChildInputs.addDistanceValueCommandInput('distanceValue', 'DistanceValue', adsk.core.ValueInput.createByReal(2))
-            # distanceValueInput.setManipulator(adsk.core.Point3D.create(0, 0, 0), adsk.core.Vector3D.create(1, 0, 0))
-            # distanceValueInput.minimumValue = 0
-            # distanceValueInput.isMinimumValueInclusive = True
-            # distanceValueInput.maximumValue = 10
-            # distanceValueInput.isMaximumValueInclusive = True
-            # distanceValueInput.initialValue = 5
-
-            
-            # # Create distance value input 2.
-            # distanceValueInput2 = tab2ChildInputs.addDistanceValueCommandInput('distanceValue2', 'DistanceValue 2', adsk.core.ValueInput.createByReal(1))
-            # distanceValueInput2.setManipulator(adsk.core.Point3D.create(0, 0, 0), adsk.core.Vector3D.create(0, 1, 0))
-            # distanceValueInput2.expression = '1 in'
-            # distanceValueInput2.hasMinimumValue = False
-            # distanceValueInput2.hasMaximumValue = False
-            
-            # # # Create table input
-            # # tableInput = tab2ChildInputs.addTableCommandInput('table', 'Table', 3, '1:1:1')
-            # # addRowToTable(tableInput)
-
-            # # # Add inputs into the table.            
-            # # addButtonInput = tab2ChildInputs.addBoolValueInput('tableAdd', 'Add', False, '', True)
-            # # tableInput.addToolbarCommandInput(addButtonInput)
-            # # deleteButtonInput = tab2ChildInputs.addBoolValueInput('tableDelete', 'Delete', False, '', True)
-            # # tableInput.addToolbarCommandInput(deleteButtonInput)
-            
-            # # Create angle value input.
-            # angleValueInput = tab2ChildInputs.addAngleValueCommandInput('angleValue', 'AngleValue', adsk.core.ValueInput.createByString('30 degree'))
-            # angleValueInput.setManipulator(adsk.core.Point3D.create(0, 0, 0), adsk.core.Vector3D.create(1, 0, 0), adsk.core.Vector3D.create(0, 0, 1))
-            # angleValueInput.hasMinimumValue = False
-            # angleValueInput.hasMaximumValue = False
         except:
             ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
 
@@ -284,7 +121,7 @@ def run(context):
         # Get the existing command definition or create it if it doesn't already exist.
         cmdDef = ui.commandDefinitions.itemById('cmdInputsSample')
         if not cmdDef:
-            cmdDef = ui.commandDefinitions.addButtonDefinition('cmdInputsSample', 'Command Inputs Sample', 'Sample to demonstrate various command inputs.')
+            cmdDef = ui.commandDefinitions.addButtonDefinition('cmdInputsSample', 'Edit Parametric Part', 'Sample to demonstrate various command inputs.')
 
         # Connect to the command created event.
         onCommandCreated = MyCommandCreatedHandler()
@@ -303,7 +140,7 @@ def run(context):
 def stop(context):
     try:
         # # ui.messageBox('end')
-        globalComp.modelParameters.itemByName('Model_Param').expression = str(globalSize)
+        globalComp.modelParameters.item(1).expression = str(globalSize)
         # globalComp.attributes.add('VEX_CAD', 'Name', '2 wide C-Channel')
         # globalComp.attributes.add('VEX_CAD', 'Description', 'Words, words, more words...')
         # globalComp.attributes.add('VEX_CAD', 'Parameters', '1')
