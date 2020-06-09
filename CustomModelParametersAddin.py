6a4b328a9098b695cdcdc6bb69531c9b1f06da5b
VEXObsessed <49462576+vexobsessed@users.noreply.github.com>
2020-06-09 09:27:53 -0500

    Added inserts and improved caching

--- CustomModelParametersAddin.py
+++ CustomModelParametersAddin.py
@@ -3,6 +3,7 @@
 
 import adsk.core, adsk.fusion, adsk.cam, traceback
 import json
+import os, sys
 
 app = None
 ui  = None
@@ -19,77 +20,114 @@ globalSize = None
 _handlers = []
 
 def createAllCommandInputs(commandInputs):
-    widthHoles = commandInputs.addIntegerSliderCommandInput('widthHoles', 'Width', 1, 35)
+    widthHoles = commandInputs.addIntegerSliderCommandInput("widthHoles", "Width", 1, 35)
     widthHoles.isVisible = False
-    lengthHoles = commandInputs.addIntegerSliderCommandInput('lengthHoles', 'Length', 1, 35)
+    lengthHoles = commandInputs.addIntegerSliderCommandInput("lengthHoles", "Length", 1, 35)
     lengthHoles.isVisible = False
+    inserts = commandInputs.addDropDownCommandInput("inserts", "Inserts", 0)
+    inserts.isVisible = False
+    inserts.listItems.add("None", True)
+    inserts.listItems.add("Square", False)
+    inserts.listItems.add("Round", False)
 
 def hideAllCommandInputs():
-    inputs.itemById('widthHoles').isVisible = False
-    inputs.itemById('lengthHoles').isVisible = False
+    inputs.itemById("widthHoles").isVisible = False
+    inputs.itemById("lengthHoles").isVisible = False
+
+def setHoles(key, value):
+    thisIndexMP = value["indexMP"]
+    globalInputIndex[key] = {"value": "", "indexMP": thisIndexMP}
+
+    thisInput = inputs.itemById(key)
+    # thisInput.setText
+    # thisInput.setText(value["name"], "")
+    thisInput.minimumValue = value["minValue"]
+    thisInput.maximumValue = value["maxValue"]
+    # modelParameters.item(1).expression = str(globalSize)
+    thisInput.expressionOne = str(globalComp.modelParameters.item(thisIndexMP).expression)
+    thisInput.isVisible = True
+
+def setInserts(key, value):
+    globalInputIndex[key] = {"value": ""}
+    # listItems = add
+    thisInput = inputs.itemById(key)
+    # thisInput.expressionOne = str(globalComp.modelParameters.item(thisIndexMP).expression)
+    # globalComp.
+    thisInput.isVisible = True
 
 def setSomeCommandInputs(parameters):
     for key, value in parameters.items():
-        if key == 'lengthHoles':
-            thisIndexMP = value['indexMP']
-            globalInputIndex[thisIndexMP] = key
-
-            thisInput = inputs.itemById('lengthHoles')
-            # thisInput.setText
-            # thisInput.setText(value['name'], '')
-            thisInput.minimumValue = value['minValue']
-            thisInput.maximumValue = value['maxValue']
-            # modelParameters.item(1).expression = str(globalSize)
-            thisInput.expressionOne = str(globalComp.modelParameters.item(thisIndexMP).expression)
-            thisInput.isVisible = True
-        elif key == 'widthHoles':
-            thisIndexMP = value['indexMP']
-            globalInputIndex[thisIndexMP] = key
-
-            thisInput = inputs.itemById('widthHoles')
-            # thisInput.setText
-            # thisInput.setText(value['name'], '')
-            thisInput.minimumValue = value['minValue']
-            thisInput.maximumValue = value['maxValue']
-            # modelParameters.item(1).expression = str(globalSize)
-            thisInput.expressionOne = str(globalComp.modelParameters.item(thisIndexMP).expression)
-            thisInput.isVisible = True
-
+        if key == "lengthHoles":
+            setHoles(key, value)
+        elif key == "widthHoles":
+            setHoles(key, value)
+        elif key == "inserts":
+            setInserts(key, value)
 
 def updateInputs():
     global globalComp
     global globalParameters
 
-    selectionInput = inputs.itemById('selection')
-    if cmdInput.id == 'selection':
+    selectionInput = inputs.itemById("selection")
+    if cmdInput.id == "selection":
         hideAllCommandInputs()
         globalParameters.clear()
         if selectionInput.selectionCount > 0:
             if app.activeProduct.rootComponent != selectionInput.selection(0).entity:
                 globalComp = selectionInput.selection(0).entity.component
-                if globalComp and globalComp.attributes.count > 0 and globalComp.attributes.itemByName('pVFL', 'part name').isValid:
-                    setSomeCommandInputs(json.loads(globalComp.attributes.itemByName('pVFL', 'parameters').value))
+                if globalComp and globalComp.attributes.count > 0 and globalComp.attributes.itemByName("pVFL", "data"):
+                    # print(json.loads(globalComp.attributes.itemByName("pVFL", "data").value)["parameters"])
+                    setSomeCommandInputs(json.loads(globalComp.attributes.itemByName("pVFL", "data").value)["parameters"])
                 else:
                     selectionInput.clearSelection()
             else:
                 selectionInput.clearSelection()
     else:
         for key, value in globalInputIndex.items():
-            globalParameters[key] = inputs.itemById(value).expressionOne
+            print(globalInputIndex)
+            globalParameters[key] = globalInputIndex[key]
+            if inputs.itemById(key).classType() == "adsk::core::DropDownCommandInput":
+                globalParameters[key] = {"value": inputs.itemById(key).selectedItem.name}
+            elif inputs.itemById(key).classType() == "adsk::core::IntegerSliderCommandInput":
+                globalParameters[key]["value"] = inputs.itemById(key).expressionOne
+            pass
+            print(globalParameters)
+
 
     # sizeInput.isVisible = True
-    # if cmdInput.id == 'widthHoles':
+    # if cmdInput.id == "widthHoles":
     global globalSize
-    globalSize = inputs.itemById('lengthHoles')
+    globalSize = inputs.itemById("lengthHoles")
 
-def updateParameters():
-    for key, value in globalParameters.items():
-        globalComp.modelParameters.item(key).expression = value
-        # globalComp.modelParameters.item(key).expression = inputs.itemById(value).expressionOne
-        # global inputs
-        # print(str(globalSize.expressionOne))
-        # print(str(inputs.itemById('lengthHoles').expressionOne))
-        pass
+def updateModelParameter(comp, parameters):
+    comp.modelParameters.item(parameters["indexMP"]).expression = parameters["value"]
+
+def updateBodyBulbs(comp, parameters):
+    for key, value in parameters.items():
+        comp.bRepBodies.itemByName(key).isLightBulbOn = value
+
+def updateInserts(comp, parameters):
+    # print(parameters)
+    if parameters["value"] == "None":
+        updateBodyBulbs(comp, {"Square Insert 1": False, "Square Insert 2": False, "Round Insert 1": False, "Round Insert 2": False})
+    elif parameters["value"] == "Square":
+        updateBodyBulbs(comp, {"Square Insert 1": True, "Square Insert 2": True, "Round Insert 1": False, "Round Insert 2": False})
+    elif parameters["value"] == "Round":
+        updateBodyBulbs(comp, {"Square Insert 1": False, "Square Insert 2": False, "Round Insert 1": True, "Round Insert 2": True})
+
+
+def updatePart(comp, parameterList):
+    for key, value in parameterList.items():
+        if key == "lengthHoles":
+            updateModelParameter(comp, value)
+        elif key == "widthHoles":
+            updateModelParameter(comp, value)
+        elif key == "inserts":
+            updateInserts(comp, value)
+
+    # updateModelParameter(globalComp, {"lengthHoles": {"value": "12", "indexMP": 1}})
+    # updateModelParameter(globalComp, globalParameters)
+    # updateInserts(globalComp, globalParameters)
 
 
 
@@ -112,7 +150,7 @@ class MyCommandInputChangedHandler(adsk.core.InputChangedEventHandler):
             updateInputs()
 
         except:
-            ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
+            ui.messageBox("Failed:\n{}".format(traceback.format_exc()))
 
 
 # Event handler that reacts to when the command is destroyed. This terminates the script.            
@@ -125,7 +163,7 @@ class MyCommandDestroyHandler(adsk.core.CommandEventHandler):
             # This will release all globals which will remove all event handlers
             adsk.terminate()
         except:
-            ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
+            ui.messageBox("Failed:\n{}".format(traceback.format_exc()))
 
 
 # Event handler that reacts when the command definition is executed which
@@ -153,23 +191,23 @@ class MyCommandCreatedHandler(adsk.core.CommandCreatedEventHandler):
             inputs = cmd.commandInputs
 
             # Create a tab input.
-            # tabCmdInput1 = inputs.addTabCommandInput('tab_1', 'Tab 1')
+            # tabCmdInput1 = inputs.addTabCommandInput("tab_1", "Tab 1")
             editParametricInputs = inputs
 
             # Create a selection input.
-            selectionInput = editParametricInputs.addSelectionInput('selection', 'Select Parametric Part', 'Component to select')
+            selectionInput = editParametricInputs.addSelectionInput("selection", "Select Parametric Part", "Component to select")
             selectionInput.setSelectionLimits(1,1)
             selectionFilter = selectionInput.addSelectionFilter("Occurrences")
             # selectionInput.selectionFilters = selectionFilter
 
             # Create integer slider input with one slider.
-            # sliderOne = editParametricInputs.addIntegerSliderCommandInput('widthHoles', 'Length', 1, 35)
+            # sliderOne = editParametricInputs.addIntegerSliderCommandInput("widthHoles", "Length", 1, 35)
             # sliderOne.isVisible = False
 
             createAllCommandInputs(editParametricInputs)
 
         except:
-            ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
+            ui.messageBox("Failed:\n{}".format(traceback.format_exc()))
 
 
 def run(context):
@@ -178,10 +216,10 @@ def run(context):
         app = adsk.core.Application.get()
         ui = app.userInterface
 
-        # Get the existing command definition or create it if it doesn't already exist.
-        cmdDef = ui.commandDefinitions.itemById('cmdInputsSample')
+        # Get the existing command definition or create it if it doesn"t already exist.
+        cmdDef = ui.commandDefinitions.itemById("cmdInputsSample")
         if not cmdDef:
-            cmdDef = ui.commandDefinitions.addButtonDefinition('cmdInputsSample', 'Edit Parametric Part', 'Sample to demonstrate various command inputs.')
+            cmdDef = ui.commandDefinitions.addButtonDefinition("cmdInputsSample", "Edit Parametric Part", "Sample to demonstrate various command inputs.")
 
         # Connect to the command created event.
         onCommandCreated = MyCommandCreatedHandler()
@@ -193,41 +231,65 @@ def run(context):
 
         # Prevent this module from being terminated when the script returns, because we are waiting for event handlers to fire.
         adsk.autoTerminate(False)
+
+        # importManager = app.importManager
+        # rootComp = app.activeProduct.rootComponent
+        # # Get archive import options
+        # archiveFileName = "C:\\library/Test C-Channel.f3d"
+        # archiveOptions = importManager.createFusionArchiveImportOptions(archiveFileName)
+        
+        # # Import archive file to root component
+        # importManager.importToTarget(archiveOptions, rootComp)
+
+
     except:
         if ui:
-            ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
+            ui.messageBox("Failed:\n{}".format(traceback.format_exc()))
 
 def stop(context):
     try:
-        
-        # # ui.messageBox('end')
-        # globalComp.modelParameters.item(1).expression = str(globalSize)
-        # globalComp.attributes.add('VEX_CAD', 'Name', '2 wide C-Channel')
-        # globalComp.attributes.add('VEX_CAD', 'Description', 'Words, words, more words...')
-        # globalComp.attributes.add('VEX_CAD', 'Parameters', '1')
-        # print('globalComp.attributes.count: ' + str(globalComp.attributes.count))
-        # print(str(globalComp.attributes.itemsByGroup('pVFL')[0].name))
-        # testAttribute = str(globalComp.attributes.itemByName('VEX_CAD', 'parameters').value)
-        # # globalComp.modelParameters.itemByName('Model_Param').expression = testAttribute
-        # print(testAttribute)
-
-        updateParameters()
+        updatePart(globalComp, globalParameters)
 
         # print(str(globalComp.attributes.count))
         # print(str(globalComp.attributes.groupNames))
-        # print(str(globalComp.attributes.itemByName('pVFL', 'part name').value))
-        # print(str(globalComp.attributes.itemByName('pVFL', 'parametric').value))
-        # print(str(globalComp.attributes.itemByName('pVFL', 'parameters').value))
+        # print(str(globalComp.attributes.itemByName("pVFL", "part name").value))
+        # print(str(globalComp.attributes.itemByName("pVFL", "parametric").value))
+        # print(str(globalComp.attributes.itemByName("pVFL", "parameters").value))
 
-        # globalComp.attributes.add('pVFL', 'part name', 'Al 1x2x1x35 C-Channel v1')
-        # globalComp.attributes.add('pVFL', 'parametric', '1')
-        # globalComp.attributes.add('pVFL', 'parameters', '{"lengthHoles": {"name": "Length", "indexMP": 1, "minValue": 1, "maxValue": 35} }')
+        # globalComp.attributes.add("pVFL", "part name", "Al 1x2x1x35 C-Channel v1")
+        # globalComp.attributes.add("pVFL", "parametric", "1")
+        # globalComp.attributes.add("pVFL", "parameters", "{"lengthHoles": {"name": "Length", "indexMP": 1, "minValue": 1, "maxValue": 35} }")
 
-        # globalComp.attributes.add('pVFL', 'part name', 'Al 1x25 Plate v1')
-        # globalComp.attributes.add('pVFL', 'parametric', '1')
-        # globalComp.attributes.add('pVFL', 'parameters', '{"widthHoles": {"name": "Width", "indexMP": 0, "minValue": 1, "maxValue": 5}, "lengthHoles": {"name": "Length", "indexMP": 1, "minValue": 1, "maxValue": 25} }')
+        # globalComp.attributes.add("pVFL", "part name", "Al 1x25 Plate v1")
+        # globalComp.attributes.add("pVFL", "parametric", "1")
+        # globalComp.attributes.add("pVFL", "parameters", "{"widthHoles": {"name": "Width", "indexMP": 0, "minValue": 1, "maxValue": 5}, "lengthHoles": {"name": "Length", "indexMP": 1, "minValue": 1, "maxValue": 25} }")
+        
+        # globalComp.attributes.add("pVFL", "data",
+        # """{
+        #     "partName": "Al 1x2x1x35 C-Channel v1",
+        #     "isParametric": "1",
+        #     "parameters": {
+        #         "lengthHoles": {
+        #             "indexMP": 1,
+        #             "minValue": 1,
+        #             "maxValue": 35
+        #         }
+        #     }
+        # }""")
+
+        # globalComp.attributes.add("pVFL", "data",
+        # """{
+        #     "partName": "HS 36t Gear v1",
+        #     "isParametric": "1",
+        #     "parameters": {
+        #         "inserts": {
+        #             "squareBodies": ["Square Insert 1", "Square Insert 2"],
+        #             "roundBodies": ["Round Insert 1", "Round Insert 2"]
+        #         }
+        #     }
+        # }""")
         pass
 
     except:
         if ui:
-            ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
+            ui.messageBox("Failed:\n{}".format(traceback.format_exc()))
